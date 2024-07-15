1da6904dded2ded31196586370bbf8b279d378d2
Param Birje <parambirje@gmail.com>
2024-07-15 19:46:40 +0530

    Form now actually adds data to local storage

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+let form = document.querySelector("#form-part");
+
+// Description:
+// This script is responsible for handling the form submission
+// and storing the data in the local storage.
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  // structuring the form data
+  let formData = new FormData(form);
+  let data = {};
+  for (let [key, value] of formData.entries()) {
+    data[key] = value;
+  }
+
+  // append the data to the list in local storage
+  // if the list is not present, create a new list
+  let existingData = localStorage.getItem("turbomarks-data");
+  if (existingData) {
+    existingData = JSON.parse(existingData);
+  } else {
+    existingData = [];
+  }
+  existingData.push(data);
+  localStorage.setItem("turbomarks-data", JSON.stringify(existingData));
+});
