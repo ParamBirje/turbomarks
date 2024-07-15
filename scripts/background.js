9c0db6a418c704970da5c5abdcb7e0d6037f92c5
Param Birje <parambirje@gmail.com>
2024-07-15 21:58:11 +0530

    Added a rough suggestions algo to service worker

--- scripts/background.js
+++ scripts/background.js
@@ -1,15 +1,31 @@
-chrome.omnibox.onInputChanged.addListener((text, suggest) => {
-  console.log("User input:", text);
+const localStorageKey = "turbomarks-data";
+let turbomarksData = [];
 
-  // Provide suggestions to the closest match
-  suggest([
-    { content: text + " one", description: "the first one" },
-    { content: text + " number two", description: "the second entry" },
-  ]);
+// As the input changes, provide suggestions
+chrome.omnibox.onInputChanged.addListener((text, suggest) => {
+  let suggestions = [];
+
+  // Start a loop to iterate over the turbomarksData list
+  // and find the shorthand that starts with the input text
+  for (let i of turbomarksData) {
+    let shorthand = i.shorthand;
+    let url = i.url;
+
+    // If the title starts with the input text
+    // provide the suggestion
+    if (shorthand.startsWith(text)) {
+      suggestions.push({
+        content: shorthand,
+        description: url,
+      });
+    }
+  }
+
+  suggest(suggestions);
 });
 
+// On pressing enter, open the matched URL
 chrome.omnibox.onInputEntered.addListener((text) => {
-  // Input the matched URL into the url bar
   let newURL = "https://www.google.com";
   chrome.tabs.update({ url: newURL });
 });
